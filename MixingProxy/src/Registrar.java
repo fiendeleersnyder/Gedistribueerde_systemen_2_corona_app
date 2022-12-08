9dc6bf9489ee715c2fa810c8d93114789e1a4342
fiendeleersnyder <fien.deleersnyder@student.kuleuven.be>
2022-12-08 09:34:20 +0100

    correct keystore added

--- MixingProxy/src/Registrar.java
+++
@@ -1,6 +0,0 @@
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-
-public interface Registrar {
-    PublicKey getPublicKey(String phone_number) throws NoSuchAlgorithmException;
-}
