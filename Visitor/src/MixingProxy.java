aa966daa532b118b72e3086d31b5b7360aa75948
EmmyValckenier <emmy.valckenier@gmail.com>
2022-12-09 17:25:30 +0100

    Changes by Fien

--- Visitor/src/MixingProxy.java
+++ Visitor/src/MixingProxy.java
@@ -8,7 +8,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public interface MixingProxy extends Remote {
-    byte[] sendCapsule(Capsule capsule, String phoneNumber) throws RemoteException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException;
+    byte[] sendCapsule(Capsule capsule) throws RemoteException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException;
 
     void sendInfectedTokens(ArrayList<usedToken> infectedInformed) throws RemoteException;
 }
